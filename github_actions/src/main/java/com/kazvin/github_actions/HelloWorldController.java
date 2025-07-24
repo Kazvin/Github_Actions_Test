e646fe747710a818c007d05aa2b82bdcfa00a35d
kazvin mendez <kazvin.mendez@fdmgroup.com>
2025-07-23 23:43:55 -0400

    initial project

---
+++ github_actions/src/main/java/com/kazvin/github_actions/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.kazvin.github_actions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
